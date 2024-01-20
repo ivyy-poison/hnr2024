0e7d74bfaf4d36192d4a47f22a948b85f0e1ef1c
HR Leong <hrleonghk@gmail.com>
2024-01-21 01:28:00 +0800

    final commit tonight

--- audio.js
+++
@@ -1,9 +0,0 @@
-import { AudioContext } from "web-audio-api";
-
-const audioContext = new AudioContext();
-
-const oscillator = audioContext.createOscillator();
-
-oscillator.connect(audioContext.destination);
-
-oscillator.start(0);
