28743b76a34e89af123d7826728e6619428f88c8
Ivan <103909775+ivyy-poison@users.noreply.github.com>
2024-01-20 23:39:48 +0800

    Add user input component

---
+++ components/InputBlock.tsx
@@ -0,0 +1,27 @@
+interface Props {
+    text: string;
+    onChange?: (value: string) => void;
+    onSubmit?: () => void;
+  }
+
+export const InputBlock: React.FC<Props> = ({
+    text,
+    onChange = () => {},
+    onSubmit = () => {},
+  }) => {
+    function handleKeyPress(event: React.KeyboardEvent) {
+      if (event.ctrlKey && event.key === 'Enter') {
+        onSubmit()
+      }
+    }
+    return (
+      <textarea
+        className="min-h-100px w-full bg-[#1A1B26] text-sm focus:outline-none"
+        style={{ resize: 'none', fontFamily: 'Source Code Pro, monospace' }}
+        value={text}
+        placeholder="Enter user input here ..."
+        onChange={(e) => onChange(e.target.value)}
+        onKeyDown={handleKeyPress}
+      />
+    );
+  };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { CodeBlock } from '@/components/CodeBlock';
 import Header from '@/components/Header';
 import { TextBlock } from '@/components/TextBlock';
+import { InputBlock } from '@/components/InputBlock';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -9,12 +10,14 @@ export default function Home() {
 
   const [inputCode, setInputCode] = useState<string>('');
   const [outputCode, setOutputCode] = useState<string>('');
+  const [userInput, setUserInput] = useState<string>('');
   const [date, setDate] = useState<Date>(new Date());
 
   function handleTranslate() {
     axios.post('/api/translate', {
       date: formatDate(date),
       code: inputCode,
+      input: userInput
     })
     .then((response) => {
       console.log(response)
@@ -46,7 +49,7 @@ export default function Home() {
         <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
           <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
             <div className="text-center text-xl font-bold">Input</div>
-            
+
               <CodeBlock
                 code={inputCode}
                 editable={true}
@@ -55,6 +58,9 @@ export default function Home() {
                 }}
                 onSubmit={handleTranslate}
               />
+
+              <InputBlock text={userInput} onChange={setUserInput} onSubmit={handleTranslate} />
+            
             
           </div>
           <div className="mt-8 flex h-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
