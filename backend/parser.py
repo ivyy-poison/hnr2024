9d691706ce6210e5bc38160bd5fed2612e1c31f4
jedkohjk <jedkoh2001@gmail.com>
2024-01-20 19:56:14 +0800

    Add parsing exceptions

--- backend/parser.py
+++ backend/parser.py
@@ -5,7 +5,20 @@ def parse(filename):
         codelist = [list(i.split()) for i in f.read().split('\n') if len(i.replace(' ', ''))]
     for instruction in codelist:
         insts = [i for i, j in instructions[len(instruction) - 1]]
-        instruction[0] = insts.index(instruction[0].upper())
+        try:
+            instruction[0] = insts.index(instruction[0].upper())
+        except ValueError:
+            raise InvalidCommandError()
         for i in range(len(instruction)):
-            instruction[i] = int(instruction[i])
+            try:
+                instruction[i] = int(instruction[i])
+            except ValueError:
+                raise InvalidCommandFormat()
     return codelist
+
+class InvalidCommandError(Exception):
+    pass
+
+class InvalidCommandFormat(Exception):
+    pass
+
