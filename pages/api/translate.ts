fcd63ce1523fe3eb73e2f4604eb90e1497d9073b
Ivan <103909775+ivyy-poison@users.noreply.github.com>
2024-01-20 21:03:30 +0800

    Implement backend running Python scripts and returning output/error

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -1,63 +1,49 @@
 import { spawn } from 'child_process';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
-// function POST(request: Request) {
-//   return request.json().then(({ code, date }) => {
-//     // Stub method to process the code and date
-//     const output = interpretCode(code, date);
-//     return new Response(output, { status: 200 });
-//   }).catch((err) => {
-//     return new Response(`something went wrong ${err}`, { status: 500 });
-//   });
-// }
-function POST(req: NextApiRequest, res: NextApiResponse) {
+async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { code, date } = req.body;
-  const output = interpretCode(code, date);
-  res.status(200).send(output);
+  interpretCode(code, date).then((result) => {
+    res.status(200).send(result);
+  }).catch((error) => {
+    res.status(404).send(error);
+  });
 }
 
-function interpretCode(code: string, date: string) {
-  
-  // Run a Python script using spawn
+async function interpretCode(code: string, date: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('python3', ['backend/test.py', '123', 'testing']);
 
-  // const pythonProcess = spawn('python', ['../../backend/test.py']);
+    let scriptOutput = '';
+    pythonProcess.stdout.on('data', (data) => {
+      scriptOutput += data.toString();
+    });
 
-  // // Collect data from script
-  // let scriptOutput = '';
-  // pythonProcess.stdout.on('data', (data) => {
-  //   scriptOutput += data.toString();
-  // });
+    let scriptError = '';
+    pythonProcess.stderr.on('data', (data) => {
+      scriptError += data.toString();
+    });
 
-  // // Collect error/exception messages
-  // let scriptError = '';
-  // pythonProcess.stderr.on('data', (data) => {
-  //   scriptError += data.toString();
-  // });
-
-  // // Handle script completion
-  // pythonProcess.on('close', (code) => {
-  //   if (code !== 0 || scriptError) {
-  //     console.error(`Python script failed with code ${code} and error: ${scriptError}`);
-  //     // res.status(500).json({ error: scriptError });
-      
-  //   } else {
-  //     console.log('Python script executed successfully:', scriptOutput);
-  //     // res.status(200).json({ result: scriptOutput });
-  //   }
-  // });
-
-  return `Code: ${code}, Date: ${date}`;
+    pythonProcess.on('close', (statusCode) => {
+      if (statusCode !== 0 || scriptError) {
+        console.error(`Python script failed with code ${statusCode} and error: ${scriptError}`);
+        reject(`Python script failed with code ${statusCode} and error: ${scriptError}`);
+      } else {
+        console.log('Python script executed successfully:', scriptOutput);
+        resolve(`Code: ${code}, status: ${statusCode}, Date: ${date}, scriptOutput: ${scriptOutput}, scriptError: ${scriptError}`);
+      }
+    });
+  });
 }
 
 function GET(request: NextApiRequest, response: NextApiResponse) {
   response.status(200).send("Hello World!");
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    return POST(req, res);
+    POST(req, res);
   } else {
-    return GET(req, res);
+    GET(req, res);
   }
 }
